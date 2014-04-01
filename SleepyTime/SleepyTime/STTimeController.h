a416bd208c3b2e4beb7aca96d904928ca3e09579
pblanco <petercblanco@gmail.com>
2014-04-01 11:34:25 -0400

    add basic layout

---
+++ SleepyTime/SleepyTime/STTimeController.h
@@ -0,0 +1,13 @@
+//
+//  STTimeController.h
+//  SleepyTime
+//
+//  Created by Peter  Blanco on 4/1/14.
+//  Copyright (c) 2014 Blancotech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STTimeController : NSObject
+
+@end
---
+++ SleepyTime/SleepyTime/STTimeController.m
@@ -0,0 +1,13 @@
+//
+//  STTimeController.m
+//  SleepyTime
+//
+//  Created by Peter  Blanco on 4/1/14.
+//  Copyright (c) 2014 Blancotech. All rights reserved.
+//
+
+#import "STTimeController.h"
+
+@implementation STTimeController
+
+@end
